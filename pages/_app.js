34c931cde685f1af3063b90e49cf00610cfcfab6
vedpra55 <vedna400@gmail.com>
2022-08-28 13:26:05 +0530

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import { Toaster } from "react-hot-toast";
-
+import { useRouter } from "next/router";
 import { CartProvider } from "react-use-cart";
 import { useEffect, useState } from "react";
 import { AuthStateContext } from "../context/authContext";
@@ -8,9 +8,19 @@ import Layout from "../components/layout/layout";
 import ProgressBar from "@badrap/bar-of-progress";
 import Router from "next/router";
 
+const progress = new ProgressBar({
+  size: 3,
+  color: "red",
+  delay: 100,
+});
+
+Router.events.on("routeChangeStart", progress.start);
+Router.events.on("routeChangeComplete", progress.finish);
+Router.events.on("routeChangeError", progress.finish);
+
 function MyApp({ Component, pageProps }) {
   const [isClient, setClient] = useState(false);
-
+  const router = useRouter();
   useEffect(() => setClient(true), []);
 
   if (!isClient) return null;
