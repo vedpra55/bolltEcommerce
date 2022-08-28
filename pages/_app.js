1772b9cbf055689ced086d85a2347a716b0d333c
vedpra55 <vedna400@gmail.com>
2022-08-28 13:22:57 +0530

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,8 @@ import { CartProvider } from "react-use-cart";
 import { useEffect, useState } from "react";
 import { AuthStateContext } from "../context/authContext";
 import Layout from "../components/layout/layout";
+import ProgressBar from "@badrap/bar-of-progress";
+import Router from "next/router";
 
 function MyApp({ Component, pageProps }) {
   const [isClient, setClient] = useState(false);
@@ -12,13 +14,22 @@ function MyApp({ Component, pageProps }) {
   useEffect(() => setClient(true), []);
 
   if (!isClient) return null;
+  const progress = new ProgressBar({
+    size: 3,
+    color: "black",
+    delay: 100,
+  });
+
+  Router.events.on("routeChangeStart", progress.start);
+  Router.events.on("routeChangeComplete", progress.finish);
+  Router.events.on("routeChangeError", progress.finish);
 
   return (
     <CartProvider>
       <AuthStateContext>
         <Toaster />
         <Layout>
-          <Component {...pageProps} />
+          <Component {...pageProps} key={router.asPath} />
         </Layout>
       </AuthStateContext>
     </CartProvider>
